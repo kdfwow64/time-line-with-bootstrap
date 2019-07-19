d1cca872e6a5033ff3448c2a1d09bcc2351520fe
kdfwow <kdfwow0422@gmail.com>
2019-07-19 19:31:26 +0200

    first commit

---
+++ build/precache-manifest.f6349eb653a35506ce777020cebf25ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "b308848b6663f032b7d57e6b35c7a783",
+    "url": "/static/media/Cabin-Regular.b308848b.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c549ca6037d4e793cc91",
+    "url": "/static/js/main.6fee9205.chunk.js"
+  },
+  {
+    "revision": "0e523e2826d695f4e4b0",
+    "url": "/static/js/2.1826a7f7.chunk.js"
+  },
+  {
+    "revision": "c549ca6037d4e793cc91",
+    "url": "/static/css/main.ffb27cd9.chunk.css"
+  },
+  {
+    "revision": "0e523e2826d695f4e4b0",
+    "url": "/static/css/2.ef11d5fb.chunk.css"
+  },
+  {
+    "revision": "0f1d2ccbf2baf98071982161dae3aa25",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react'
+import Granim from 'react-granim'
+import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+const data = [{"id":1,"days_until":0,"description":"Enjoy an all-night celebration with access to the Moresocial VIP area.","happens_at":"Saturday, July 20 @ 12AM","hours_until":16,"name":"All-Night Rooftop Concert","purchasable":true,"url":"/demo/concert","slug":"concert","venue":{"id":1,"address":"45 Rockefeller Plaza, New York, NY","description":null,"fee_rate":0.03,"latitude":40.7583395,"longitude":-73.9792303,"name":"Pier 19","picture_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Profiles/Concert.jpg","slug":"demo","tax_rate":0.08875,"url":"/demo"},"media":[{"media_type":"video","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/VideoPreview/Concert.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Video/concert.webm"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-2.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-2.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-6.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-6.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-1.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-1.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-8.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-8.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-4.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Concert/Moresocial-Live-Demo-4.jpg"}],"packages":[{"id":1,"description":"VIP Access to the Moresocial section and lounge area","name":"VIP","price_in_cents":4500,"fee_fixed_amount_in_cents":0},{"id":2,"description":"Duis doctrina fidelissimae se cernantur sunt excepteur. Arbitror iis quorum ad officia aute duis senserit noster.","name":"General Admission","price_in_cents":1000,"fee_fixed_amount_in_cents":0}]},{"id":2,"days_until":2,"description":"Bottomless waterside brunch all afternoon.","happens_at":"Sunday, July 21 @  1PM","hours_until":5,"name":"Bottomless Sunday Brunch","purchasable":true,"url":"/demo/bottomless-brunch","slug":"bottomless-brunch","venue":{"id":1,"address":"45 Rockefeller Plaza, New York, NY","description":null,"fee_rate":0.03,"latitude":40.7583395,"longitude":-73.9792303,"name":"Pier 19","picture_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Profiles/Concert.jpg","slug":"demo","tax_rate":0.08875,"url":"/demo"},"media":[{"media_type":"video","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/VideoPreview/Brunch.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Brunch/brunch.webm"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Brunch/Moresocial-Live-Demo-Brunch-1.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Brunch/Moresocial-Live-Demo-Brunch-1.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Brunch/Moresocial-Live-Demo-Brunch-2.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Brunch/Moresocial-Live-Demo-Brunch-2.jpg"}],"packages":[{"id":3,"description":"Enjoy unlimited food and amazing music all afternoon","name":"Bottomless","price_in_cents":3000,"fee_fixed_amount_in_cents":0}]},{"id":3,"days_until":2,"description":"An exhilarating ride with priceless views of Manhattan and the Statue of Liberty.","happens_at":"Sunday, July 21 @  1PM","hours_until":5,"name":"Statue of Liberty Flight Tour","purchasable":true,"url":"/demo/flight-tour","slug":"flight-tour","venue":{"id":1,"address":"45 Rockefeller Plaza, New York, NY","description":null,"fee_rate":0.03,"latitude":40.7583395,"longitude":-73.9792303,"name":"Pier 19","picture_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Profiles/Concert.jpg","slug":"demo","tax_rate":0.08875,"url":"/demo"},"media":[{"media_type":"video","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/VideoPreview/Flight.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/flight.webm"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-1.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-1.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-2.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-2.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-3.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-3.jpg"},{"media_type":"photo","preview_url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-4.jpg","url":"https://storage.googleapis.com/live.moresocial.ly/Demo/Flight/Moresocial-Live-Demo-Flight-4.jpg"}],"packages":[{"id":4,"description":"Friday, July 5 flight package for (2) guests","name":"July 5","price_in_cents":20000,"fee_fixed_amount_in_cents":0},{"id":5,"description":"Saturday, July 6 flight package for (2) guests","name":"July 6","price_in_cents":20000,"fee_fixed_amount_in_cents":0}]}];
+
+for(var i=0;i<data.length;i++) {
+	data[i]['short_date'] = data[i]['happens_at'].match(/, (.*?) @/)[1];
+	var date_array = data[i]['short_date'].split(' ');
+	data[i]['short_date'] = date_array[1]+' '+date_array[0];
+}
+data.sort(function(a, b){
+    if(a.short_date < b.short_date) return -1;
+    if(a.short_date > b.short_date) return 1;
+    return 0;
+});
+var temp = -1;
+var grouped_data = [];
+var group = [];
+for(i=0;i<data.length;i++) {
+	if(temp!=data[i]['short_date']) {
+		temp = data[i]['short_date'];
+		if("short_date" in group) {
+			grouped_data.push(group);
+			group = [];
+		}
+		group['short_date'] = data[i]['short_date'];
+		group['temp'] = [];
+		group['temp'].push(data[i]);
+	} else {
+		group['temp'].push(data[i]);
+		if(i == (data.length-1))
+			grouped_data.push(group);
+	}
+}
+const granim_gradient_options = {
+	"default-state": {
+		"gradients": [
+			['#0732A7', '#6845A0'],
+			['#6845A0', '#F1815F'],
+			['#F1815F', '#0732A7']
+		],
+		"transitionSpeed": 7000
+	}
+}
+
+const granim_direction_options = {
+	"x0": '100%',
+	'y0': '40%',
+	'x1': '0%',
+	'y1': '60%'
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      	<div className="App" style={{ height: '100vh' }}>
+			<div style={{ position: 'fixed'}} className="background">
+				<Granim direction="diagonal" states={granim_gradient_options} customDirection={granim_direction_options} />
+			</div>
+			<div className="MainContent">
+				<Container>
+					{/* <VerticalTimeline layout={'1-column'}>
+						{
+							grouped_data.map((item, index) => {
+								return (
+									<VerticalTimelineElement
+										className="vertical-timeline-element--work"
+										iconStyle={{ background: 'transparent' }}
+										icon={item.short_date}
+									>
+										{
+											item.temp.map((it, idx) => {
+												return (
+													<div className="video-element" style={{ backgroundImage: "url("+it.media[0].preview_url+")" }}>
+														<div className="camera-div">
+															<FontAwesomeIcon icon={faFacebook} className="camera-icon"></FontAwesomeIcon>
+														</div>
+														<div className="txt-div">
+															<div className="big-txt">
+																{it.venue.address}
+															</div>
+															<div className="small-txt">
+																{it.happens_at.replace(' @', ',')}
+															</div>
+														</div>
+													</div>
+												)
+											})
+										}
+									</VerticalTimelineElement>
+								)
+							})
+						}
+					</VerticalTimeline> */}
+					<Row>
+						{
+							grouped_data.map((item, index) => {
+								if(index == 0)
+									return (
+										<Col xs="4" className="text-align-span">
+											<span className="circle-mobile active">{item.short_date}</span>
+										</Col>
+									)
+								else
+									return (
+										<Col xs="4" className="text-align-span">
+											<span className="circle-mobile">{item.short_date}</span>
+										</Col>
+									)
+							})
+						}
+					</Row>
+					{
+						grouped_data.map((item, index) => {
+							return (
+								<Row
+									className="each-date-row"
+								>
+									<div
+										className="timeline-div"
+									>
+										<span className="circle">{item.short_date}</span>
+									</div>
+									<Row
+										className="inner-row"
+									>
+										{
+											item.temp.map((it, idx) => {
+												return (
+													<Col md="4" className="one-third-item">
+														<div className="video-element-div" style={{ backgroundImage: "url("+it.media[0].preview_url+")" }}>
+															<div className="camera-div">
+																<FontAwesomeIcon icon={faFacebook} className="camera-icon"></FontAwesomeIcon>
+															</div>
+															<div className="txt-div">
+																<div className="big-txt">
+																	{it.venue.address}
+																</div>
+																<div className="small-txt">
+																	{it.happens_at.replace(' @', ',')}
+																</div>
+															</div>
+														</div>
+													</Col>
+												)
+											})
+										}
+									</Row>
+								</Row>
+							)
+						})
+					}
+				</Container>
+			</div>
+      	</div>
+    );
+  }
+}
+
+export default App;
